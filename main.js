85c22cd9e2cea334acfe5f45fe057192b61c0b58
Matias Flores <437632@student.fontys.nl>
2022-06-12 04:05:01 +0200

    Leaderboard tryout

--- main.js
+++ main.js
@@ -1,6 +1,122 @@
 import './style.css'
+import * as firebase from './src/firebase.js';
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+
+const app = document.querySelector("#app") 
+const table = document.querySelector("#ex-table")
+// let isSortAsc = false
+
+//////////////API//////////////////
+
+// const students = []
+
+
+
+Promise.resolve(firebase.getStudents()).then((values) => {
+  // app.innerHTML+=values; 
+  // console.log(values);
+
+const students = values
+
+sortDesc()
+// isSortAsc = !isSortAsc;
+
+document.getElementById("desc").addEventListener("click", () => {
+  students.reverse()
+  renderTable(students)
+});
+
+
+// function sortAsc(){
+// students.sort((s1, s2)=> s1.points >= s2.points)
+// renderTable(students)
+// isSortAsc = true
+// }
+
+function sortDesc(){
+students.sort((s1, s2)=> s1.points >= s2.points).reverse()
+// isSortAsc = false
+}
+renderTable(students)
+
+
+function renderTable(students){
+
+  table.innerHTML= `<thead class="thead-inverse">
+          <tr>
+            <th>Rank</th>
+            <th>Name</th>
+            <th><i class="fa-solid fa-sort"></i> Points</th>
+          </tr>
+        </thead>`;
+  
+  const tbody = document.createElement('tbody')
+
+
+  for(let index in students){
+    // app.innerHTML+= values[index].name
+    const row = document.createElement('tr')
+    const rank = document.createElement('td')
+    rank.innerText = index
+    const name = document.createElement('td')
+    name.innerText = values[index].name
+    const points = document.createElement('td')
+    points.innerText = values[index].points
+    row.appendChild(rank)
+    row.appendChild(name)
+    row.appendChild(points)
+    tbody.appendChild(row)
+  }
+  table.appendChild(tbody)
+}
+});
+
+
+
+  let nameInput = document.querySelector("#name").value;
+  let pointsInput = document.querySelector("#points").value;
+
+  if (nameInput !== "" && pointsInput !== "") {
+    firebase.writeUserData(nameInput, pointsInput);
+  }
+  nameInput.value = ""
+  pointsInput.value = ""
+  const submitButton = document.querySelector("#submit");
+  submitButton.addEventListener("click", () => {
+  window.location.href = window.location.href;
+  });
+
+// console.log(students);
+
+// console.log(typeof students)
+
+  //   students.forEach(element => {
+  //   console.log(element)
+  //   app.innerHTML+= element;
+  // });
+
+
+// console.log( document.getElementById("app"))
+
+// renderPage()
+// firebase.writeUserData()
+
+// var database = ;
+
+// database.once('value', function(snapshot){
+//     if(snapshot.exists()){
+//         var content = '';
+
+        // students.forEach(function(data){
+        //     var Name = data.val().name;
+        //     var Points= data.val().points;
+        //     content += '<tr>';
+        //     content += '<td>' + Name + '</td>'; //column1
+        //     content += '<td>' + Points + '</td>';//column2
+        //     content += '</tr>';
+        // });
+
+        // ('#ex-table').append(content);
+//     }
+// });
+
---
+++ src/firebase.js
@@ -0,0 +1,70 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, addDoc, doc, getDoc, query, where, getDocs, orderBy } from "firebase/firestore";
+
+// We get the keys from our .env file. You can use these files to store
+// sensitive information so you don't put this in git.
+// Ideally you'll want this stored in the backend so it doesn't get
+// outputted to the frontend entirely.
+// Learn more here: https://www.freecodecamp.org/news/heres-how-you-can-actually-use-node-environment-variables-8fdf98f53a0a/
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: `${import.meta.env.VITE_FIREBASE_APIKEY}`,
+  authDomain: `${import.meta.env.VITE_FIREBASE_AUTHDOMAIN}`,
+  projectId: `${import.meta.env.VITE_FIREBASE_PROJECTID}`,
+  databaseURL: `${import.meta.env.VITE_FIREBASE_DATABASEURL}`,
+  storageBucket: `${import.meta.env.VITE_FIREBASE_STORAGEBUCKET}`,
+  messagingSenderId: `${import.meta.env.VITE_FIREBASE_MESSASINGSENDERID}`,
+  appId: `${import.meta.env.VITE_FIREBASE_APPID}`,
+};
+
+console.log(firebaseConfig);
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+export const writeUserData = async (name, points) => {
+  console.log("send data");
+
+  const docRef = await addDoc(collection(db, "Students"), {
+        name: name,
+        points: points
+        });
+  
+}
+
+export const getStudents = async () =>{
+//   console.log("get data");
+//   // Get the value the users has put in
+
+
+//   // Check in the console if it's right
+  // console.log(nameInput, professionInput);
+
+//   // Check if the input is empty or not. If not = send data to database
+  
+
+    // const q = query(collection(db, "Students"), where("name", "==", "Winnie the Pooh"));
+    const students = []
+    const querySnapshot = await getDocs(collection(db, "Students"), orderBy("points"));
+      querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        students.push(doc.data())
+      });
+  return students
+}
+
+
+
+
+
+
+
+// const readData = function readData() {
+
+
+// }
+// export default readData
